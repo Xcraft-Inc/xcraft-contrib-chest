415a09122c5fb35af4a8d23aa20b16e02982c718
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-01 13:39:04 +0200

    Use functions in order to init and get the global busClient.
    By this way, we are sure that only some specific modules will
    create the global busClient.
    
    Here, it's only zog and the server. In all other cases, the global
    handle must be just a getter.

--- chest.js
+++ chest.js
@@ -6,7 +6,7 @@ var path = require ('path');
 var fs   = require ('fs');
 
 var xLog        = require ('xcraft-core-log') (moduleName);
-var busClient   = require ('xcraft-core-busclient').global;
+var busClient   = require ('xcraft-core-busclient').getGlobal ();
 var chestConfig = require ('xcraft-core-etc').load ('xcraft-contrib-chest');
 
 var cmd = {};
